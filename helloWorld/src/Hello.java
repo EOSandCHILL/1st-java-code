fd1d3fd8f7c6a460cd637be2963ee9726b62b644
MackInTech <94152169+EOSandCHILL@users.noreply.github.com>
2022-05-17 21:48:33 -0400

    Add files via upload

---
+++ helloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+/* Public is an access modifier.
+Static is a java keyword that we need for java to find our method in
+order to run the code we are going to add.
+Void is another java keyword used to indicate that the method will not return anything.
+The parenthesis in a method declaration are mandatory & can optionally
+include one or more parameters which is a way to pass information to a method.
+The {} represents a code block which defines a block of code. This is mandatory.
+println should now print out the statement "Hello World"
+ */
